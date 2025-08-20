b6989cec3f268e31d7d1c1ee9a8afaa595600e83
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-08-20 11:35:15 -0300

    Aula 34 finalizada!

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -6,9 +6,15 @@ public class Aula07Arrays02 {
         // char -> '\u0000' = ' '
         // boolean -> false
         // String -> null
-        String[] nomes = new String[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        String[] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+
+        for (int i = 0; i < nomes.length ; i++) {
+            System.out.println(nomes[i]);
+        }
+
+        nomes = new String[5];
     }
 }
