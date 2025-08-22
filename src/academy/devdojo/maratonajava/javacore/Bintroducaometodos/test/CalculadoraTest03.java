16bce6743c78b9dcdc35c20faa4a4746a9c6a7ed
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-08-22 11:26:25 -0300

    Aula 47 (Métodos 04) Concluída

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -5,7 +5,11 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        double result = calculadora.divideDoisNumeros(20,0);
+        double result = calculadora.divideDoisNumeros(20, 0);
         System.out.println(result);
+        System.out.println(calculadora.divideDoisNumeros2(20, 0));
+        System.out.println("------------------------------");
+        calculadora.imprimeDivisaoDeDoisNumeros(86,0);
+
     }
 }
