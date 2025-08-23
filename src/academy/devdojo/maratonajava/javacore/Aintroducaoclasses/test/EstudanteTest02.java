0e17ea2ab43a72a6f375f74d6d716907dd76bc09
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-08-23 03:35:44 -0300

    Aula 49 (Métodos 06) Concluída

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest02.java
@@ -11,7 +11,9 @@ public class EstudanteTest02 {
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
+
         System.out.println("--------------------------------");
+
         System.out.println(estudante2.nome);
         System.out.println(estudante2.idade);
         System.out.println(estudante2.sexo);
