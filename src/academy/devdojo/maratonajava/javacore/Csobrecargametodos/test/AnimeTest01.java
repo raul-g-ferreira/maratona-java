dccb276cbcfc95e318355d1927a2817fb2527737
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-08-28 17:36:01 -0300

    Aula 57 (Sobrecarga de métodos) Concluída

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("Akudama Drive", "TV", 12);
+        anime.init("Akudama Drive 2", "TV", 12, "Ação");
+        anime.setGenero("Ação");
+        anime.imprime();
+    }
+}
