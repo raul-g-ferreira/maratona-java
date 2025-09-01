c356ac2ea9cf63d9db2c4467a430f8abc80b5a76
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-09-01 17:28:36 -0300

    Aula 63 (Métodos de inicialização estáticos) Concluída

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -0,0 +1,53 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    /**
+     * 0 - Bloco de inicialização é executado quando a JVM carregar a classe
+     * 1 - Alocado espaço em memória pro objeto
+     * 2 - Cada atributo de classe é criado e incializado com valores default ou o que for passado
+     * 3 - Bloco de inicializacão é executado
+     * 4 - Construtor é executado
+     */
+    static {
+        System.out.println("Dentro do bloco de inicialização estático1");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicialização estático2");
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicialização estático3");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicialização não estático");
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+
+        for (int episodio : Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
