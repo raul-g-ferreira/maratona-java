2c454892979e0051a0129283f9c26f155fa19cdd
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-09-09 06:41:56 -0300

    Aula 71 (Herança 01) Concluída

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Seminario.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Seminario.java
@@ -5,10 +5,6 @@ public class Seminario {
     private Aluno[] alunos;
     private Local local;
 
-    public Seminario(String titulo) {
-        this.titulo = titulo;
-    }
-
     public Seminario(String titulo, Local local) {
         this.titulo = titulo;
         this.local = local;
---
+++ src/academy/devdojo/maratonajava/javacore/HHeranca/test/HerancaTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.HHeranca.test;
+
+import academy.devdojo.maratonajava.javacore.HHeranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.HHeranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.HHeranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("324098-383");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyohisa Shimazu");
+        pessoa.setCpf("11111111123");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        System.out.println("---------------------------");
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("1209312");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(200000);
+        funcionario.imprime();
+    }
+}
