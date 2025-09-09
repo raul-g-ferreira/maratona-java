fb1b049b305897daf4255a1df953c6b1497a513d
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-09-09 06:56:23 -0300

    Aula 72 (Herança 02) Concluída

--- src/academy/devdojo/maratonajava/javacore/HHeranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/HHeranca/dominio/Funcionario.java
@@ -3,6 +3,10 @@ package academy.devdojo.maratonajava.javacore.HHeranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
     public double getSalario() {
         return salario;
     }
