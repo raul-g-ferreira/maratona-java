fea79a6cf87d962b2fefb815e2e5caf39ddbf185
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-09-17 16:52:24 -0300

    Aula 86 (Classes abstratas 03) Concluída

--- src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio;
 
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa{
     protected String nome;
     protected double salario;
 
@@ -10,4 +10,17 @@ public abstract class Funcionario {
         calculaBonus();
     }
     public abstract void calculaBonus();
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo...");
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
 }
