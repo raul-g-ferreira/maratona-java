6b009cb402ac20168a56a788240300c8b1f19283
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-10-14 16:45:55 -0300

    Aula 116 (Locale)

---
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/LocaleTest02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao.test;
+
+import java.util.Locale;
+
+public class LocaleTest02 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        String[] isoCountries = Locale.getISOCountries();
+        String[] isoLanguages = Locale.getISOLanguages();
+        for (String isoLanguage : isoLanguages) {
+            System.out.println(isoLanguage + " ");
+        }
+        for (String isoCountry : isoCountries) {
+            System.out.println(isoCountry + " ");
+        }
+
+
+    }
+}
