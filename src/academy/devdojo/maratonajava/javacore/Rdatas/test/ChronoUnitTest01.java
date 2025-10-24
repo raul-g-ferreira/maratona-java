5973324d2120c4aa47c123e684864e8f187a9116
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-10-24 15:29:40 -0300

    Aula 125 (ChronoUnit)

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/ChronoUnitTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(2007, Month.MAY,4,6,45,0);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
@@ -17,5 +18,9 @@ public class PeriodTest01 {
         System.out.println(p2);
         System.out.println(p3);
         System.out.println(p4);
+        System.out.println(p5);
+        System.out.println(p3.getMonths());
+        System.out.println(Period.between(now, now.plusDays(p3.getDays())).getMonths());
+        System.out.println(now.until(now.plusDays(p3.getDays()), ChronoUnit.MONTHS));
     }
 }
