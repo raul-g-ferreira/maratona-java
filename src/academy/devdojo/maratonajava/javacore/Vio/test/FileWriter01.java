80b5003d54f182c776e1977dec2c82a9b4502c35
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-11-06 00:29:21 -0300

    Aula 140 (FileWriter)

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file,true)){
+            fw.write("O devdojo é lindo, é o melhor curso do brasillll\n Continuando a cantoria na proxima linha\n");
+            fw.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
