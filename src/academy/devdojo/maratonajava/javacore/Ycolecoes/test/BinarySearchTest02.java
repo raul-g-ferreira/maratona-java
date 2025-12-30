b53d7f19389c8dbd2453bce8a77951bd8afccfcd
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-12-30 15:23:58 -0300

    172 - Coleções pt 12 - Binary Search

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/BinarySearchTest02.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BinarySearchTest02 {
+    public static void main(String[] args) {
+        MangaByIdComparator mangaByIdComparator = new MangaByIdComparator();
+        List<Manga> mangas =  new ArrayList<>(6);
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9));
+        mangas.add(new Manga(1L, "Pokemon", 9.5));
+        mangas.add(new Manga(4L, "Berserk", 3.2));
+        mangas.add(new Manga(3L, "Attack on Titan", 11.20));
+        mangas.add(new Manga(2L, "Dragon Ball Z", 2.99));
+
+//        Collections.sort(mangas);
+        mangas.sort(mangaByIdComparator);
+        for (Manga manga : mangas){
+            System.out.println(manga);
+        }
+
+        Manga mangatoSearch = new Manga(2L, "Dragon Ball Z", 2.99);
+
+        System.out.println(Collections.binarySearch(mangas, mangatoSearch, mangaByIdComparator));
+    }
+
+}
